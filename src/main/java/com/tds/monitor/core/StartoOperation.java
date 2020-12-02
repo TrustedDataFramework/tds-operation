99898b936617bbc3ea81709050e8878ef03dff09
YunMin <347594248@qq.com>
2020-12-02 10:52:21 +0800

    update

---
+++ src/main/java/com/tds/monitor/core/StartoOperation.java
@@ -0,0 +1,13 @@
+package com.tds.monitor.core;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartoOperation {
+
+    public StartoOperation(){
+        //查看本地节点是否启动
+        //没有启动，查看java version是否安装，没有安装异常，终止启动，正常到指定默认路径shell启动节点
+        //查看浏览器是否启动
+    }
+}
---
+++ src/main/java/com/tds/monitor/utils/URLUtil.java
@@ -0,0 +1,46 @@
+package com.tds.monitor.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class URLUtil {
+
+    public RestTemplate restTemplate;
+
+    private SimpleClientHttpRequestFactory simpleClientHttpRequestFactory;
+
+    public URLUtil() {
+        this.simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+        simpleClientHttpRequestFactory.setReadTimeout(8 * 1000);
+        simpleClientHttpRequestFactory.setConnectTimeout(10 * 1000);
+        this.restTemplate = new RestTemplate(this.simpleClientHttpRequestFactory);
+    }
+
+
+    //查看节点是否启动 本地节点默认端口7010
+    public JSONObject getNodeInfo(String url, long port) {
+        url = "http://" + url + ":" + port + "/rpc/stat";
+        JSONObject result;
+        try {
+            result = restTemplate.getForObject(url, JSONObject.class);
+        } catch (Exception e) {
+            return null;
+        }
+        return result;
+    }
+
+    //查看浏览器后端是否启动，默认端口8181
+    public String getBrowserInfo(String url, long port) {
+        url = "http://" + url + ":" + port + "";
+        String result;
+        try {
+            result = restTemplate.getForObject(url, String.class);
+        } catch (Exception e) {
+            return null;
+        }
+        return result;
+    }
+}
